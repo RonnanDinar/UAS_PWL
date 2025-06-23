03b3142434a212ed89ad335e648bc49cd092ba6e
mee0412 <daffaridya@gmail.com>
2025-06-23 16:40:42 +0700

    menambahkan migrasi and seeding, merubah env, menambahkan register

---
+++ belajar-ci/app/Database/Migrations/2025-06-23-084824_Mobil.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Mobil extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama_mobil' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => true,
+            ],
+            'merk' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+                'null'       => true,
+            ],
+            'nopol' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 60,
+                'null'       => true,
+            ],
+            'harga_sewa' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+                'null'       => true,
+            ],
+            'status' => [
+                'type'       => 'ENUM',
+                'constraint' => ['tersedia', 'disewa'],
+                'default'    => 'tersedia',
+                'null'       => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('mobil');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('mobil');
+    }
+}
